47883e3cee1bb80274bde83543410f1a48db0893
Kihoon Han <khhan1993@gmail.com>
2017-04-17 21:24:49 +0900

    Deal with carrage return character problem

--- assignment2/data_read.cpp
+++ assignment2/data_read.cpp
@@ -8,45 +8,45 @@
 
 #include "assignment2.hpp"
 
+string deleteCarrageReturn(string S);
+
 vector< vector< pair<string, string> > > getTrainingData(string &train_path) {
     ifstream ifs(train_path, ifstream::in);
     
     string str;
+    
     getline(ifs, str);
+    str = deleteCarrageReturn(str);
     
     string token;
+    string delimiter = "\t";
+    auto pos = string::npos;
     vector<string> attribute_names;
     
-    for(auto &elem: str) {
-        if(elem == '\t' || elem == '\n' || elem == '\r') {
-            attribute_names.push_back(token);
-            token.clear();
-        }
-        else {
-            token.push_back(elem);
-        }
+    while((pos = (int)str.find(delimiter)) != string::npos) {
+        token = str.substr(0, pos);
+        attribute_names.push_back(token);
+        str.erase(0, pos + delimiter.length());
     }
-    token.clear();
+    attribute_names.push_back(str);
     
     vector< vector<string> > attribute_values;
     while(true) {
         vector<string> tmp_list;
+        
         getline(ifs, str);
+        str = deleteCarrageReturn(str);
         
         if(ifs.eof()) {
             break;
         }
         
-        for(auto &elem: str) {
-            if(elem == '\t' || elem == '\n' || elem == '\r') {
-                tmp_list.push_back(token);
-                token.clear();
-            }
-            else {
-                token.push_back(elem);
-            }
+        while((pos = (int)str.find(delimiter)) != string::npos) {
+            token = str.substr(0, pos);
+            tmp_list.push_back(token);
+            str.erase(0, pos + delimiter.length());
         }
-        token.clear();
+        tmp_list.push_back(str);
         
         attribute_values.push_back(tmp_list);
     }
@@ -69,41 +69,39 @@ vector< vector< pair<string, string> > > getTestData(string &test_path) {
     ifstream ifs(test_path, ifstream::in);
     
     string str;
+    
     getline(ifs, str);
+    str = deleteCarrageReturn(str);
     
     string token;
+    string delimiter = "\t";
+    auto pos = string::npos;
     vector<string> attribute_names;
     
-    for(auto &elem: str) {
-        if(elem == '\t' || elem == '\n' || elem == '\r') {
-            attribute_names.push_back(token);
-            token.clear();
-        }
-        else {
-            token.push_back(elem);
-        }
+    while((pos = (int)str.find(delimiter)) != string::npos) {
+        token = str.substr(0, pos);
+        attribute_names.push_back(token);
+        str.erase(0, pos + delimiter.length());
     }
-    token.clear();
+    attribute_names.push_back(str);
     
     vector< vector<string> > attribute_values;
     while(true) {
         vector<string> tmp_list;
+        
         getline(ifs, str);
+        str = deleteCarrageReturn(str);
         
         if(ifs.eof()) {
             break;
         }
         
-        for(auto &elem: str) {
-            if(elem == '\t' || elem == '\n' || elem == '\r') {
-                tmp_list.push_back(token);
-                token.clear();
-            }
-            else {
-                token.push_back(elem);
-            }
+        while((pos = (int)str.find(delimiter)) != string::npos) {
+            token = str.substr(0, pos);
+            tmp_list.push_back(token);
+            str.erase(0, pos + delimiter.length());
         }
-        token.clear();
+        tmp_list.push_back(str);
         
         attribute_values.push_back(tmp_list);
     }
@@ -120,4 +118,15 @@ vector< vector< pair<string, string> > > getTestData(string &test_path) {
     }
     
     return test_data;
+}
+
+string deleteCarrageReturn(string S) {
+    string carrage_return = "\r";
+    auto pos = string::npos;
+    
+    while((pos = (int)S.find(carrage_return)) != string::npos) {
+        S.erase(pos, pos + carrage_return.length());
+    }
+    
+    return S;
 }
